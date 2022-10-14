c8c84cd9cf9b56ca6ad3c67ca44bf72aa997e257
ivmarkov <ivan.markov@gmail.com>
2022-10-14 14:00:13 +0300

    mapper

--- src/asynch.rs
+++ src/asynch.rs
@@ -1,5 +1,6 @@
 use core::fmt::Debug;
 use core::future::Future;
+use core::marker::PhantomData;
 
 pub mod mpmc;
 pub mod pubsub;
@@ -57,5 +58,57 @@ where
 
     fn recv(&mut self) -> Self::RecvFuture<'_> {
         async move { (*self).recv().await }
+    }
+}
+
+pub struct Mapper<C, F, Q>(C, F, PhantomData<fn() -> Q>);
+
+impl<C, F, Q> Mapper<C, F, Q> {
+    pub const fn new(channel: C, mapper: F) -> Self {
+        Self(channel, mapper, PhantomData)
+    }
+}
+
+impl<C, F, Q> Sender for Mapper<C, F, Q>
+where
+    C: Sender,
+    F: Fn(Q) -> Option<C::Data>,
+{
+    type Error = C::Error;
+
+    type Data = Q;
+
+    type SendFuture<'a> = impl Future<Output = Result<(), Self::Error>> where Self: 'a;
+
+    fn send(&mut self, data: Self::Data) -> Self::SendFuture<'_> {
+        async move {
+            if let Some(data) = (self.1)(data) {
+                self.0.send(data).await
+            } else {
+                Ok(())
+            }
+        }
+    }
+}
+
+impl<C, F, Q> Receiver for Mapper<C, F, Q>
+where
+    C: Receiver,
+    F: Fn(C::Data) -> Option<Q>,
+{
+    type Error = C::Error;
+
+    type Data = Q;
+
+    type RecvFuture<'a> = impl Future<Output = Result<Self::Data, Self::Error>> where Self: 'a;
+
+    fn recv(&mut self) -> Self::RecvFuture<'_> {
+        async move {
+            loop {
+                if let Some(data) = (self.1)(self.0.recv().await?) {
+                    return Ok(data);
+                }
+            }
+        }
     }
 }
