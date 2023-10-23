5a3358c19d17a0ac8abe6e567d7441f405c2645b
ivmarkov <ivan.markov@gmail.com>
2023-10-23 08:39:07 +0000

    Wrong features

--- src/asynch.rs
+++ src/asynch.rs
@@ -5,11 +5,7 @@ pub mod mpmc;
 pub mod notification;
 pub mod pubsub;
 pub mod signal;
-#[cfg(any(
-    feature = "ws-edge-net",
-    feature = "ws-embedded-svc",
-    feature = "ws-wasm"
-))]
+#[cfg(any(feature = "edge-net", feature = "embedded-svc", feature = "wasm"))]
 pub mod ws;
 
 pub trait Sender {
